2ddcbac9edd4d696a94fdd041189b14b9273a52a
GuilFranca <guilfranca27@gmail.com>
2024-11-13 11:13:56 -0300

    att hoste

---
+++ js/script.js
@@ -0,0 +1,24 @@
+// Função para calcular o IMC
+function calculateIMC(event) {
+    event.preventDefault();
+
+    const weight = parseFloat(document.getElementById("weight").value);
+    const height = parseFloat(document.getElementById("height").value);
+
+    if (weight > 0 && height > 0) {
+        const imc = weight / (height * height);
+        let resultText;
+
+        if (imc < 18.5) {
+            resultText = "Você está abaixo do peso.";
+        } else if (imc >= 18.5 && imc <= 24.9) {
+            resultText = "Você está com o peso ideal.";
+        } else {
+            resultText = "Você está acima do peso.";
+        }
+
+        document.getElementById("result").textContent = `Seu IMC é ${imc.toFixed(2)}. ${resultText}`;
+    } else {
+        document.getElementById("result").textContent = "Por favor, insira valores válidos.";
+    }
+}
